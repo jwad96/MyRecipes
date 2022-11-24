d98a905f9ebbd887f5f31d5a5e459682ed50408a
Jwad <jwad.aziz@gmail.com>
2022-11-23 18:50:56 -0800

    corrected imports and added model inhertiance from db.Model

--- app/models/note.py
+++ app/models/note.py
@@ -1,12 +1,12 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .user import User
 
-class note:
+class Note(db.Model):
     __tablename__ = "notes"
 
     id = db.Column(db.Integer, primary_key = True)
-    note_author_id = db.Column(db.Integer)
-    note_recipe_id = db.Column(db.Integer)
+    note_author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    note_recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
     note = db.Column(db.String(1000))
 
 
--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,11 +1,11 @@
 from .db import db, SCHEMA, environment, add_prefix_for_prod
-from .user import user
+from .user import User
 
-class Recipe:
+class Recipe(db.Model):
     __tablename__ = "recipes"
 
     id = db.Column(db.Integer, primary_key=True)
-    recipe_author_id = db.Column(db.Integer, nullable=False)
+    recipe_author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     title = db.Column(db.String(255))
     description = db.Column(db.String(255))
     ingredients = db.Column(db.String)
