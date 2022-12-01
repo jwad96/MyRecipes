77a1be7da2f5df65f378c47bb98428a83ac42c80
Jwad <jwad.aziz@gmail.com>
2022-11-30 21:17:44 -0800

    add splash image and header

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
+import HomeSplash from "./components/HomeSplash/HomeSplash"
 import { authenticate } from './store/session';
 
 function App() {
@@ -32,7 +33,7 @@ function App() {
           <LoginForm />
         </Route>
         <Route path='/' exact={true} >
-          <h1>My Home Page</h1>
+          <HomeSplash />
         </Route>
         <Route path="/sign-up" exact={true} >
           <SignUpForm />
---
+++ react-app/src/components/HomeSplash/HomeSplash.js
@@ -0,0 +1,9 @@
+import "./HomeSplash.css"
+
+export default function HomeSplash() {
+    return (
+        <div id="home-splash">
+            <h1 id="home-splash-header">LET'S GET COOKIN</h1>
+        </div>
+    )
+}
