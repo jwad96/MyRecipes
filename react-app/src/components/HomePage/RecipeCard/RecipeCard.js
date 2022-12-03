af03e2deb337847e260455b8e4def82c53078960
Jwad <jwad.aziz@gmail.com>
2022-12-03 14:23:17 -0800

    complete recipe title section ' '

--- react-app/src/components/HomePage/RecipeCard/RecipeCard.js
+++ react-app/src/components/HomePage/RecipeCard/RecipeCard.js
@@ -3,7 +3,7 @@ import {useHistory} from "react-router";
 import "./RecipeCard.css"
 
 export default function RecipeCard({title, authorName, previewImageURL, recipeId}) {
-    const backgroundStyle = {"backgroundImage": `url(${previewImageURL})`}
+    const backgroundStyle = {backgroundImage: `url(${previewImageURL})`}
 
     const history = useHistory()
 
--- react-app/src/components/RecipePage/Title/Title.js
+++ react-app/src/components/RecipePage/Title/Title.js
@@ -1,11 +1,15 @@
 import "./Title.css"
 
 export default function Title({title, image, author, description}) {
+    const backgroundStyle = {backgroundImage: `url(${image})`}
+
     return (
         <div id="title-container">
-            <div id="title-title">{title}</div>
-            <div id="title-author">{author}</div>
-            <div id="title-image">{image}</div>
+            <div id="title-title-container">
+              <div id="title-title">{title}</div>
+              <div id="title-author">by {author}</div>
+            </div>
+            <div id="title-image" style={backgroundStyle} />
             <p id="title-description">{description}</p>
         </div>
     )
