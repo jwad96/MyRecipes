96d071b1bd002a51d06d1f8e361caa228ee96a1c
Jwad <jwad.aziz@gmail.com>
2022-12-05 00:45:53 -0800

    load default image on recipe card

--- react-app/src/components/HomePage/RecipeCard/RecipeCard.js
+++ react-app/src/components/HomePage/RecipeCard/RecipeCard.js
@@ -1,9 +1,24 @@
+import {useState} from "react";
 import {useHistory} from "react-router";
 
 import "./RecipeCard.css"
 
 export default function RecipeCard({title, authorName, previewImageURL, recipeId}) {
-    const backgroundStyle = {backgroundImage: `url(${previewImageURL})`}
+    const [imageURL, setImageURL] = useState(previewImageURL);
+
+    const img = new Image();
+    img.onload = function() {
+        if (this.width > 0) {
+            return;
+        }
+    }
+    img.onerror = function() {
+        setImageURL("https://images.unsplash.com/photo-1631898040032-da1a5a87d13b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")
+    }
+
+    img.src = imageURL;
+
+    const backgroundStyle = {backgroundImage: `url(${imageURL})`}
 
     const history = useHistory()
 
