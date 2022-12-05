b8895ce0b1ce647d9ab0048297a638d913c9d7c5
Jwad <jwad.aziz@gmail.com>
2022-12-04 21:03:35 -0800

    add readme

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -3,6 +3,22 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
+def is_email(form, field):
+    email = field.data
+
+    if email.count('@') != 1:
+        raise ValidationError("Email must have exactly one @ symbol")
+
+    second_half = email[email.index('@')+1:]
+
+    email_split = email.split('@')
+    second_half_split = second_half.split('.')
+
+    check_1 = len(email_split) == 2 and email_split[0] and email_split[1]
+    check_2 = len(second_half_split) >= 2 and second_half_split[-1] and second_half_split[-2]
+
+    if not (check_1 and check_2):
+        raise ValidationError('Provide a valid email')
 
 def user_exists(form, field):
     # Checking if user exists
@@ -23,5 +39,5 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired(), user_exists, is_email])
     password = StringField('password', validators=[DataRequired()])
