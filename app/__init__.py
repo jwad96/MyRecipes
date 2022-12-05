1697132885b5af2bd7ecd15df2ed7e81e0f47178
Jwad <jwad.aziz@gmail.com>
2022-12-05 00:38:11 -0800

    create migration

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-from .models import db, User
+from .models import db, User, Note, Recipe
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.note_routes import note_routes
@@ -33,7 +33,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(note_routes, url_prefix='/api/notes')
 app.register_blueprint(recipe_routes, url_prefix='/api/recipes')
 db.init_app(app)
-Migrate(app, db)
+Migrate(app, db, compare_type=True)
 
 # Application Security
 CORS(app)
